5a1823293835872c1fb336bf9da3c404416982eb
Zil Norvilis <zilasino27@gmail.com>
2022-11-01 13:45:58 +0100

    Add table controller functionality base

---
+++ app/controllers/concerns/tables_helper.rb
@@ -0,0 +1,16 @@
+module TablesHelper
+  def create_from_api_data(show, ratings)
+    show = Show.find_or_create_by(title: show[:title], imdb_id: show[:imdb_id])
+    ratings = ratings.payload
+    # debugger
+    ratings.each do |season, episodes|
+      season = Season.create(number: season, show: show)
+      
+      episodes.each do |episode|
+        Episode.create(imdb_rating: episode, season: season)
+      end
+    end
+
+    show
+  end
+end
--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,7 +1,23 @@
 class TablesController < ApplicationController
+  include TablesHelper
+
   def show
-    @result = MoviesDatabaseApiService.new({
-      imdb_id: params[:id]
-    }).call
+    name = params[:query]&.downcase
+    @show_data = Show.where("title ilike ?", "%#{name}%").first
+
+    unless @show_data
+      show_data = MovieDatabaseAlternativeApiService.new({query: params[:query]}).get_imdb_id
+      ratings_data = MoviesDatabaseApiService.new({imdb_id: show_data[:imdb_id]}).get_seasons_episodes_ratings
+      
+      @show_data = create_from_api_data(show_data, ratings_data)
+    else
+      @show_data
+    end
   end
+
+  # private
+
+  #   def table_params
+  #     params.fetch(:table, {}).permit(:query)
+  #   end
 end
